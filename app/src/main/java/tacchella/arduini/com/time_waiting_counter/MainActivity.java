3dc3331cab7a974202f52c976d8031036c21e187
Alessio Arduini <alessio@MBP-di-Alessio.station>
2018-02-04 13:25:20 +0100

    fix permission

--- app/src/main/java/tacchella/arduini/com/time_waiting_counter/MainActivity.java
+++ app/src/main/java/tacchella/arduini/com/time_waiting_counter/MainActivity.java
@@ -1,20 +1,31 @@
 package tacchella.arduini.com.time_waiting_counter;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    String speed = "0.0";
-
-    static final LocationListener locationListener = new LocationListener() {
+    static float speed = (float) 0.0;
+    //String speedUnitOfMeasure = getResources().getString(R.string.speed_unit_of_measure);
+    private static Context mContext;
+    TextView speedView;
+    final LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
-            location.getSpeed();
+
+            speed = location.getSpeed();
+            speedView.setText(Math.round(speed * 360)/100 + " " + getString(R.string.speed_unit_of_measure));
         }
 
         @Override
@@ -36,8 +47,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mContext = getApplicationContext();
         setContentView(R.layout.activity_main);
-        TextView speedView = (TextView) findViewById(R.id.speedView);
-        speedView.setText(speed);
+        speedView = (TextView) findViewById(R.id.speedView);
+        speedView.setText(Math.round(speed * 360)/100 + " " + getString(R.string.speed_unit_of_measure));
+        int permissionCheck = ContextCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+
+        }
+        else {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+
+        /*if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            Toast.makeText(this, "REQUEST LOCATION PERMISSION", Toast.LENGTH_LONG).show();
+        }*/
+
+
     }
 }
