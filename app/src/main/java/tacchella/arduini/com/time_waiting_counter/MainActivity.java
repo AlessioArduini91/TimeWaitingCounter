fc90b8e64d2cf9c7a8a7346c4b4947fd97aaa9b7
Alessio Arduini <alessio@MacBook-Pro-di-Alessio.local>
2018-02-17 14:53:40 +0100

    nuovi sviluppi e layout

--- app/src/main/java/tacchella/arduini/com/time_waiting_counter/MainActivity.java
+++ app/src/main/java/tacchella/arduini/com/time_waiting_counter/MainActivity.java
@@ -1,5 +1,7 @@
 package tacchella.arduini.com.time_waiting_counter;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -10,6 +12,8 @@ import android.view.View;
 import android.content.Context;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.content.pm.PackageManager;
 import android.location.LocationManager;
@@ -19,32 +23,64 @@ import android.Manifest;
 import android.widget.Toast;
 import android.support.annotation.NonNull;
 
-public class MainActivity extends AppCompatActivity implements ChronometerFragmentList.OnFragmentEventListener {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
 
     Chronometer timerChronometerMove, timerChronometerStop;
     Button startChronometer, resetChronometer;
     TextView speedView;
-    TextView chronoView;
+    TextView movingTextView;
+    TextView stoppingTextView;
+    TextView percentMovingTextView;
+    TextView percentStoppingTextView;
+    Chronometer movingChrono;
+    Chronometer stoppingChrono;
+    LinearLayout movingLayout;
+    LinearLayout stoppingLayout;
     final int ACCESS_FINE_LOCATION_REQUEST_CODE = 5;
     //boolean usato per scambiare tra pulsante avvio e pulsante stop
     boolean switchStartButton=true;
     //long usato per far si che dopo la pausa il pulsante ritorni a contare dal punto in cui si era fermato
     private long lastPause;
-
-    @Override
-    public void toggleChronoInfo(int toggle) {
-        Log.d("index", "ciao");
-    }
+    private int animationDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        animationDuration = getResources().getInteger(android.R.integer.config_longAnimTime);
+        movingLayout = (LinearLayout) findViewById(R.id.chronoMoveLayout);
+        stoppingLayout = (LinearLayout) findViewById(R.id.chronoStopLayout);
+        movingTextView = movingLayout.findViewById(R.id.timeText);
+        stoppingTextView = stoppingLayout.findViewById(R.id.timeText);
+        movingTextView.setId(R.id.text_view_moving);
+        stoppingTextView.setId(R.id.text_view_stopping);
+        movingChrono = movingLayout.findViewById(R.id.timeChrono);
+        stoppingChrono = stoppingLayout.findViewById(R.id.timeChrono);
+        percentMovingTextView = movingLayout.findViewById(R.id.timePercent);
+        percentStoppingTextView = stoppingLayout.findViewById(R.id.timePercent);
+
+        movingLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleVisibility(movingChrono, percentMovingTextView);
+                toggleTextView(movingChrono, movingTextView);
+            }
+        });
+
+        stoppingLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleVisibility(stoppingChrono, percentStoppingTextView);
+                toggleTextView(stoppingChrono, stoppingTextView);
+            }
+        });
 
         speedView = (TextView) findViewById(R.id.speedView);
         //inizializzo componenti
-        timerChronometerMove = findViewById(R.id.timerChronometerMove);
-        timerChronometerStop = findViewById(R.id.timerChronometerStop);
+        //timerChronometerMove = findViewById(R.id.timerChronometerMove);
+        //timerChronometerStop = findViewById(R.id.timerChronometerStop);
         startChronometer = findViewById(R.id.startChronometer);
         resetChronometer = findViewById(R.id.resetChronometer);
 
@@ -90,6 +126,38 @@ public class MainActivity extends AppCompatActivity implements ChronometerFragme
             }
         });
 
+    }
+
+    private void toggleVisibility(Chronometer chrono, TextView percent){
+
+        if (chrono.getVisibility() == View.VISIBLE){
+            chrono.setVisibility(View.GONE);
+            percent.setVisibility(View.VISIBLE);
+            percent.setAlpha(0f);
+            percent.animate()
+                    .alpha(1f)
+                    .setDuration(animationDuration)
+                    .setListener(null);
+        }
+        else{
+            percent.setVisibility(View.GONE);
+            chrono.setVisibility(View.VISIBLE);
+            chrono.setAlpha(0f);
+            chrono.animate()
+                    .alpha(1f)
+                    .setDuration(animationDuration)
+                    .setListener(null);
+        }
+
+    }
+
+    private void toggleTextView(Chronometer chrono, TextView textView) {
+        if (chrono.getVisibility() == View.VISIBLE){
+            textView.setText(R.string.timeMove);
+        }
+        else {
+            textView.setText(R.string.percentMove);
+        }
     }
 
     private void startChronometer(){
